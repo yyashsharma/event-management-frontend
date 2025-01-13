e7e7ea7116592e9a9f49fe26ca65d1067e5846fd
YashSharma001 <sharmayash4118@gmail.com>
2025-01-13 21:18:56 +0530

    fix2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://event-management-backend-7xgi.onrender.com/',
+        target: 'http://localhost:5000',
         secure: false,
       }
     }
